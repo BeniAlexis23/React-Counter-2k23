6df86aff3cd1fcd7bab447459167e6ef328d3f04
BeniAlexis23 <beni_luyo2023@outlook.com>
2023-11-29 10:47:58 -0500

    add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+
+      includeAssets: ['vite.svg'],
+
+      manifest: {
+        name: 'PWA React + Vite',
+        short_name: 'PWA-React',
+        description: 'React and vite app + PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: './public/vite.svg',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+        ],
+      },
+    }),
+  ],
+});
